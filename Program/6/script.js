0f2c5d74eba7394cab2794581ce4322484d1c2fe
Que1i0n <xandermerricks.design@gmail.com>
2022-12-29 06:14:58 +0000

    I think we finally have colours again, I should've gone to bed instead

--- Program/6/script.js
+++ Program/6/script.js
@@ -6,12 +6,28 @@ const ctx = canvas.getContext("2d");
 let prngno = fxrand();
 let diceQuant = noZero(prngno);
 
-const Phrases = getPhrases("Genesis.txt", diceQuant);
+const Phrases = getPhrases(diceQuant);
+// Convert the Phrases array to a string
+const PhrasesString = JSON.stringify(Phrases);
+
+// Convert the string back to an array
+const PhrasesArray = JSON.parse(PhrasesString);
+
+// Iterate over the elements in the PhrasesArray and extract the text between the ':' and the '\r'
+const ProcessedPhrases = PhrasesArray.map(phrase => {
+  const index = phrase.indexOf(':');
+  const processedPhrase = phrase.substring(index + 1, phrase.length - 1);
+  // Find the first space character in the processedPhrase and return the text after it
+  const spaceIndex = processedPhrase.indexOf(' ');
+  return [processedPhrase.substring(spaceIndex + 1)];
+});
+
+let palletteDepth = 3;
 
 let globalHexColors = [];
 
-const Pallettes = [];
-let palletteDepth = 3;
+let Pallettes = generateColors();
+
 const ProportionChance = getStringLengths(Phrases);
 const blendModes = ['normal', 'multiply', 'color-dodge', 'color-burn', 'hard-light', 'soft-light', 'difference'];
 const blendMode = blendModes[Math.floor(prngno * blendModes.length)];
@@ -22,22 +38,18 @@ var minutes = Math.floor(elapsedTime / 60000);
 var seconds = Math.floor((elapsedTime % 60000) / 1000);
 
 
-function noZero(prngno) {
-  // Split prngno into two parts, one before and one after the decimal point
-  const parts = String(prngno).split('.');
-  // If there is a part after the decimal point, assign it to the digits variable
-  // Otherwise, assign an empty array to the digits variable
-  const digits = parts[1] ? parts[1].split('') : [];
 
-  // Iterate through the digits and return the first non-zero digit as an integer
+
+let hexColorArrays = phraseToHexColors(ProcessedPhrases);
+
+function noZero(prngno) {
+  const parts = String(prngno).split('.');
+  const digits = parts[1] ? parts[1].split('') : [];
   for (let i = 0; i < digits.length; i++) {
     if (digits[i] !== '0') {
       return parseInt(digits[i]);
     }
   }
-
-  // If no non-zero digits were found in the part after the decimal point,
-  // iterate through the characters in the part before the decimal point
   if (parts[0]) {
     const digits = parts[0].split('');
     for (let i = 0; i < digits.length; i++) {
@@ -52,12 +64,12 @@ function noZero(prngno) {
   return 1;
 }
 
-function getPhrases(filePath, number) {
+function getPhrases(number) {
   const reading = [];
   const rawFile = new XMLHttpRequest();
 
   // Open the file asynchronously
-  rawFile.open("GET", filePath, false);
+  rawFile.open("GET", "Genesis.txt", false);
 
   rawFile.onreadystatechange = function() {
     if (rawFile.readyState === 4) {
@@ -84,82 +96,158 @@ function getPhrases(filePath, number) {
   };
 
   rawFile.send(null);
-
   return reading;
 }
-// Convert a sentence into an array of hexadecimal colors
+function generateColors() {
+  console.log("Phrases:", ProcessedPhrases);
+  const colors = [];
+  for (let i = 0; i < ProcessedPhrases.length; i++) {
+    const phrase = ProcessedPhrases[i];
+    let hexColors = phraseToHexColors(ProcessedPhrases);
+    console.log("generateColors hexColors:", hexColors)
+    const colorArray = [];
+    for (let j = 0; j < palletteDepth; j++) {
+      // Select a random color from the hexColors array
+      const color = hexColors[Math.floor(fxrand() * hexColors.length)];
+      colorArray.push(colors);
+    }
+    colors.push(colorArray);
+  }
+  return colors;
+}
 
 
-function sentenceToHexColors(sentence) {
+/*
+function generateColors(phrase) {
+  const colors = [];
+  const phraseLength = phrase.length;
+  const hexColors = sentenceToHexColors(phrase);
+
+  // Select a random color from the hexColors array
+  const baseColor = hexColors[Math.floor(fxrand() * hexColors.length)];
+  colors.push(baseColor);
+
+  // Convert the base color to RGB values
+  const baseColorRGB = hexToRGB(baseColor);
+
+  // Generate the remaining colors by rotating the hue of the base color
+  for (let i = 1; i < palletteDepth; i++) {
+    const hue = (baseColorRGB.h + (30 * i)) % 360;
+    const color = hslToHex(hue, baseColorRGB.s, baseColorRGB.l);
+    colors.push(color);
+  }
+
+  return colors;
+}
+
+function hexToRGB(hex) {
+  // Extract the R, G, and B values from the hex code
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  // Convert the RGB values to HSL
+  return rgbToHSL(r, g, b);
+}
+
+function rgbToHSL(r, g, b) {
+  // Convert the RGB values to 0-1 range
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  // Find the min and max values
+  const min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+
+  // Calculate the hue, saturation, and lightness
+  let h, s, l = (max + min) / 2;
+  if (max === min) {
+    h = s = 0; // achromatic
+  } else {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+
+  // Return the HSL values as an object
+  return { h: h * 360, s: s, l: l };
+}
+
+function hslToHex(h, s, l) {
+  // Convert the HSL values to RGB
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    };
+
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  // Convert the RGB values to hex code
+  const hex = "#" + Math.round(r * 255).toString(16) + Math.round(g * 255).toString(16) + Math.round(b * 255).toString(16);
+  return hex;
+}
+*/
+
+
+function phraseToHexColors(phrases) {
   const hexColorArrays = [];
-  let currentHexColors = [];
-  let phraseLength = 0;
 
-  // Split the sentence into an array of characters
-  const chars = sentence.split('');  
+  // Iterate over the phrases in the array
+  for (let i = 0; i < phrases.length; i++) {
+    let phrase = phrases[i];
 
-  chars.forEach((char, i) => {
-    const asciiCode = char.charCodeAt(0);
-    const hexCode = asciiCode.toString(16);
-
-    // If the hexadecimal color code is a valid 6-digit code,
-    // add it to the currentHexColors array
-    if (hexCode.length === 6) {
-      currentHexColors.push('#' + hexCode);
-      phraseLength++;
+    // Convert the phrase to a string if it is not a string
+    if (typeof phrase !== 'string') {
+      phrase = phrase.toString();
     }
 
-    // If the phrase length reaches 10 characters,
-    // push the currentHexColors array to the hexColorArrays array
-    // and start a new currentHexColors array
-    if (phraseLength === 10) {
-      hexColorArrays.push(currentHexColors);
-      currentHexColors = [];
-      phraseLength = 0;
-    }
-  });
+    // Split the phrase into an array of characters
+    const chars = phrase.split('');
 
-  // If there are any remaining hexadecimal colors in the currentHexColors array,
-  // push them to the hexColorArrays array
-  if (currentHexColors.length > 0) {
+    // Iterate over the characters in the phrase
+    const currentHexColors = chars.map(char => {
+      // Convert the character to its ASCII code
+      const asciiCode = char.charCodeAt(0);
+      // Convert the ASCII code to a hexadecimal code
+      let hexCode = asciiCode.toString(16);
+
+      // Pad the hex code with leading zeros if it is less than 6 digits long
+      while (hexCode.length < 6) {
+        hexCode = '0' + hexCode;
+      }
+
+      return '#' + hexCode;
+    });
+
+    // Push the currentHexColors array to the hexColorArrays array
     hexColorArrays.push(currentHexColors);
   }
-  globalHexColors.push(...hexColorArrays);
 
   return hexColorArrays;
 }
 
 
 
-Phrases.forEach(phrase => {
-// translates the phrase into an array of hexadecimal colours
-const hexColors = sentenceToHexColors(phrase);
-// select palletteDepth number of colours from hexColors
-const shortenedColors = getRandomColors(hexColors, palletteDepth);  // <--- colour pallette depth
-// add shortenedColors to Pallettes
-Pallettes.push(shortenedColors);
-});
-function getRandomColors(colors, numColors, prngno) {
-  const shortenedColors = [];  
-  let seed = prngno;
-  function prng() {
-    seed = (seed * 16807) % 2147483647;
-    return seed / 2147483647;
-  } 
 
-  // Generate a random selection of colors
-  for (let i = 0; i < numColors; i++) {
-    const Newprngno = prng();
-    const index = Math.floor(Newprngno * Math.min(colors.length, numColors));
-    shortenedColors.push(colors[index]);
-  }
-
-  // Remove duplicates
-  return [...new Set(shortenedColors)];
-}
-
-
-// Calculate the normalized lengths of the given strings as proportions
 function getStringLengths(strings) {
 const totalLength = strings.reduce((sum, str) => sum + str.length, 0);
 const proportionSum = strings.reduce((sum, str) => sum + (str.length / totalLength), 0);
@@ -236,11 +324,11 @@ function colorCanvasVertical(ctx, Pallettes, ProportionChance, blendMode) {
   }
 }
 
-function downloadCanvas(fileName, prngno, Phrases, diceQuant, ProportionChance, Pallettes, blendMode) {
+function downloadCanvas(fileName, prngno, ProcessedPhrases, diceQuant, ProportionChance, Pallettes, blendMode) {
     
   let metadata = `fxhash:${prngno}\nPhrases:\n`;
-      for (let i = 0; i < Phrases.length; i++) {
-        metadata += `${i + 1}. ${Phrases[i]}\n`;
+      for (let i = 0; i < ProcessedPhrases.length; i++) {
+        metadata += `${i + 1}. ${ProcessedPhrases[i]}\n`;
     }
   metadata += `dice:${diceQuant}\nProportionChance:${ProportionChance}\nPallettes:${JSON.stringify(Pallettes)}\nblendMode:${blendMode}`;
 
@@ -249,7 +337,7 @@ function downloadCanvas(fileName, prngno, Phrases, diceQuant, ProportionChance, 
   const imageDataURL = canvas.toDataURL();
   const imageLink = document.createElement("a");
   imageLink.href = imageDataURL;
-  imageLink.download =  startTime, fileName;
+  imageLink.download =  fileName + "    " + startTime;  
   document.body.appendChild(imageLink);
   imageLink.click();
   document.body.removeChild(imageLink);
@@ -264,8 +352,10 @@ function downloadCanvas(fileName, prngno, Phrases, diceQuant, ProportionChance, 
   document.body.removeChild(metadataLink);
 }
 function draw(ctx, Pallettes, ProportionChance, blendMode) {
+  console.log(prngno);
     colorCanvas(ctx, Pallettes, ProportionChance, blendMode);
-    console.log("Colours1 - Done!",  "        Elapsed time: " + minutes + " minutes " + seconds + " seconds", JSON.stringify(Pallettes), console.log(JSON.stringify(globalHexColors)));
+    console.log("Colours1 - Done!",  "        Elapsed time: " + minutes + " minutes " + seconds + " seconds", "Palletes:", Pallettes);
+    console.log("ProcessedPhrases: ", ProcessedPhrases);
     //colorCanvasHorizontal(ctx, Pallettes, ProportionChance);
     colorCanvasVertical(ctx, Pallettes, ProportionChance, blendMode);
     console.log("Colouring Vertically - Done!",  "        Elapsed time: " + minutes + " minutes " + seconds + " seconds");
@@ -274,8 +364,8 @@ function draw(ctx, Pallettes, ProportionChance, blendMode) {
             ctx.scale(4, 4);
             console.log("Angle Pass:", [i], ":", "        Elapsed time: " + minutes + " minutes " + seconds + " seconds");
         }
-    console.log("fxhash():", prngno, "Phrases:", Phrases, "dice no.:", diceQuant, "ProportionChance:", ProportionChance, "Pallettes:", Pallettes, "blendMode:", blendMode);
-    downloadCanvas(fxhash, prngno, Phrases, diceQuant, ProportionChance, Pallettes, blendMode);
+    console.log("fxhash():", prngno, "Phrases:", ProcessedPhrases, "dice no.:", diceQuant, "ProportionChance:", ProportionChance, "Pallettes:", Pallettes, "blendMode:", blendMode);
+    downloadCanvas(fxhash, prngno, ProcessedPhrases, diceQuant, ProportionChance, Pallettes, blendMode);
 }
 
 
