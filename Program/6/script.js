bb89473911422484242445657ea73569f511e745
Que1i0n <xandermerricks.design@gmail.com>
2022-12-29 01:46:27 +0000

    Update random Colours to properly fill array with different colours

--- Program/6/script.js
+++ Program/6/script.js
@@ -63,51 +63,6 @@ function getPhrases(filePath, number) {
 
   return reading;
 }
-
-
-/*  OLD VERSION OF ABOVE FUNCTION //
-//injest bible, and return quantity of phrases
-function getPhrases(filePath, number) {
-const reading = [];
-const rawFile = new XMLHttpRequest();
-// Open the file asynchronously
-rawFile.open("GET", filePath, false);
-
-rawFile.onreadystatechange = function() {
-  if (rawFile.readyState === 4) {
-    if (rawFile.status === 200 || rawFile.status == 0) {
-      const textData = rawFile.responseText;
-      const rows = textData.split('\n');
-      //let startRow = 0;
-      //let skipInterval = 1;
-      // Roll the dice to determine the number of phrases to select
-      const numDice = Math.max(...number.toString().split('').map(Number));
-      const startRow = numDice > 0 ? numDice - 1 : 0;
-      const numPhrases = numDice > 0 ? numDice : 1;
-      const skipInterval = numDice > 0 ? numDice * numPhrases : 1;
-      for (let i = 0; i < numDice; i++) {
-        const roll = Math.floor(prngno * 6) + 1;
-        startRow += roll;
-        skipInterval *= roll;
-      }
-      console.log(startRow, skipInterval);
-      for (let i = 0; i < numPhrases; i++) {
-        const index = (startRow + i * skipInterval) % rows.length;
-        reading.push(rows[index]);
-      }
-    }
-  }
-};
-
-rawFile.send(null);
-
-return reading;
-
-}
-*/
-
-
-
 // Convert a sentence into an array of hexadecimal colors
 function sentenceToHexColors(sentence) {
 const hexColors = [];
@@ -134,16 +89,19 @@ return hexColors;
 }
 
 // Generate a random selection of colors from the given array of hexadecimal colors
-function getRandomColors(colors, numColors) {
-const shortenedColors = [];
-
-for (let i = 0; i < numColors; i++) {
-  const index = Math.floor(prngno * colors.length);
+function getRandomColors(colors, numColors, prngno) {
+  const shortenedColors = [];  
+  let seed = prngno;
+  function prng() {
+    seed = (seed * 16807) % 2147483647;
+      return seed / 2147483647;
+  } 
+  for (let i = 0; i < numColors; i++) {
+    const Newprngno = prng();
+    const index = Math.floor(Newprngno * Math.min(colors.length, numColors));
   shortenedColors.push(colors[index]);
   }
-return shortenedColors;
-
-//return [...new Set(shortenedColors)]; // remove duplicates
+return [...new Set(shortenedColors)]; // remove duplicates
 
 }
 
@@ -154,84 +112,25 @@ const proportionSum = strings.reduce((sum, str) => sum + (str.length / totalLeng
 // does strings.map convert the values being produced to strings, or does it leave them as numbers?
 return strings.map(str => (str.length / totalLength) / proportionSum * 100);
 }
-
-function colorCanvasHorizontal(ctx, Pallettes, ProportionChance) {
-// Calculate the total percentage of the ProportionChance array
-if (!Array.isArray(ProportionChance)) {
-  // If ProportionChance is not an array, make it one
+function colorCanvas(ctx, Pallettes, ProportionChance, blendMode) {
+  if (!Array.isArray(ProportionChance)) {
     ProportionChance = [ProportionChance];
   }
-
   const totalPercentage = ProportionChance.reduce((sum, percentage) => {
     return sum + parseInt(percentage);
   }, 0);
-
-  // Calculate the height of each segment based on the total percentage
-  const segmentHeight = canvas.height / totalPercentage; 
-
-  // Loop through each segment in the palettes array
+  const segmentSize = Math.max(canvas.width, canvas.height) / totalPercentage; 
   for (let i = 0; i < Pallettes.length; i++) {
-    // Split the palette into an array of colors
-    const colors = Pallettes[i];
-
-    // Calculate the y-coordinate and height of the segment
-    const y = i * segmentHeight;
-    const height = segmentHeight * parseInt(ProportionChance[i]);
-
-      // Loop through the x-coordinates of the canvas
-      for (let x = 0; x < canvas.width; x++) {
-          // Loop through the y-coordinates of the segment
-          for (let y = i * height; y < (i + 1) * height; y++) {
-          // Choose a random color from the palette
-          const color = colors[Math.floor(prngno * colors.length)];
-
-          // Set the fill style and draw a 1x1 pixel at the current coordinates
-          ctx.fillStyle = color;
-          ctx.fillRect(x, y, 1, 1);
-    }
+    const colors = Pallettes[i];   
+    const x = canvas.width > canvas.height ? i * segmentSize : 0;
+    const y = canvas.width > canvas.height ? 0 : i * segmentSize;
+    const width = canvas.width > canvas.height ? segmentSize * parseInt(ProportionChance[i]) : canvas.width;
+    const height = canvas.width > canvas.height ? canvas.height : segmentSize * parseInt(ProportionChance[i]);
+    ctx.globalCompositeOperation = blendMode;
+    ctx.fillStyle = colors[0];
+    ctx.fillRect(x, y, width, height);
   }
 }
-}
-
-function colorCanvasVertical(ctx, Pallettes, ProportionChance, blendMode) {
-// Calculate the total percentage of the ProportionChance array
-if (!Array.isArray(ProportionChance)) {
-  // If ProportionChance is not an array, make it one
-  ProportionChance = [ProportionChance];
-}
-const totalPercentage = ProportionChance.reduce((sum, percentage) => {
-  return sum + parseInt(percentage);
-}, 0);
-
-// Calculate the height of each segment based on the total percentage
-const segmentHeight = canvas.width / totalPercentage; 
-
-// Loop through each segment in the palettes array
-for (let i = 0; i < Pallettes.length; i++) {
-  const colors = Pallettes[i];    const y = i * segmentHeight;
-  const width = segmentHeight * parseInt(ProportionChance[i]);
-
-
-  
-  // Set the blend mode for the current segment
-  ctx.globalCompositeOperation = blendMode;
-
-  // Loop through the x-coordinates of the canvas
-  for (let y = 0; y < canvas.width; y++) {
-    // Loop through the y-coordinates of the segment
-    for (let x = i * width; x < (i + 1) * width; x++) {
-      // Choose a random color from the palette
-      const color = colors[Math.floor(prngno * colors.length)];
-
-      // Set the fill style and draw a 1x1 pixel at the current coordinates
-      ctx.fillStyle = color;
-      ctx.fillRect(x, y, 1, 1);
-    }
-  }
-}
-console.log(blendMode);
-}
-
 function colorCanvasAngled(ctx, Pallettes, ProportionChance, blendMode) {
   for (let i = 0; i < ProportionChance.length; i++) {
     const angle = (ProportionChance[i] / prngno) * 360;
@@ -256,8 +155,33 @@ function colorCanvasAngled(ctx, Pallettes, ProportionChance, blendMode) {
     }
   }
 }
+function colorCanvasVertical(ctx, Pallettes, ProportionChance, blendMode) {
+  if (!Array.isArray(ProportionChance)) {
+    ProportionChance = [ProportionChance];
+  }
+  const totalPercentage = ProportionChance.reduce((sum, percentage) => {
+    return sum + parseInt(percentage);
+  }, 0);
+  const segmentHeight = canvas.width / totalPercentage; 
+  for (let i = 0; i < Pallettes.length; i++) {
+    const colors = Pallettes[i];   
+    const y = i * segmentHeight;
+    const width = segmentHeight * parseInt(ProportionChance[i]);
+    ctx.globalCompositeOperation = blendMode;
+    for (let y = 0; y < canvas.width; y++) {
+      for (let x = i * width; x < (i + 1) * width; x++) {
+        const color = colors[Math.floor(prngno * colors.length)];
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, 1, 1);
+      }
+      // Draw the current segment to the canvas
+      ctx.drawImage(canvas, 0, 0);
+    }
+  }
+}
 
 function downloadCanvas(fileName, prngno, Phrases, diceQuant, ProportionChance, Pallettes, blendMode) {
+    
   let metadata = `fxhash:${prngno}\nPhrases:\n`;
       for (let i = 0; i < Phrases.length; i++) {
         metadata += `${i + 1}. ${Phrases[i]}\n`;
@@ -289,27 +213,27 @@ function draw(ctx, Pallettes, ProportionChance, blendMode) {
     colorCanvasVertical(ctx, Pallettes, ProportionChance, blendMode);
 
         for (let i = 0; i < 5; i++) {
-            colorCanvasAngled(ctx, Pallettes, ProportionChance, blendMode, pixelBatch);
+            colorCanvasAngled(ctx, Pallettes, ProportionChance, blendMode);
             ctx.scale(4, 4);
             console.log("Angle Pass:", [i]);
         }
     console.log("fxhash():", prngno, "Phrases:", Phrases, "dice no.:", diceQuant, "ProportionChance:", ProportionChance, "Pallettes:", Pallettes, "blendMode:", blendMode);
     downloadCanvas(fxhash, prngno, Phrases, diceQuant, ProportionChance, Pallettes, blendMode);
 }
-  
 
 const canvas = document.getElementById("canvas");
 canvas.width = 3840;  // 4K resolution
 canvas.height = 2160;
 const ctx = canvas.getContext("2d");
+
 let prngno = fxrand();
 let diceQuant = noZero(prngno);
-const Phrases = getPhrases("Genesis.txt", diceQuant);  
+
+const Phrases = getPhrases("Genesis.txt", diceQuant);
 const Pallettes = [];
 let palletteDepth = 3;
 const ProportionChance = getStringLengths(Phrases);
-//  ['normal', 'multiply', 'screen', 'overlay', 'darken', 'lighten', 'color-dodge', 'color-burn', 'hard-light', 'soft-light', 'difference', 'exclusion', 'hue', 'saturation', 'color', 'luminosity'];
-const blendModes = ['normal', 'multiply', 'color-dodge', 'color-burn', 'hard-light', 'soft-light', 'difference', 'exclusion', 'hue', 'saturation', 'luminositiy'];
+const blendModes = ['normal', 'multiply', 'color-dodge', 'color-burn', 'hard-light', 'soft-light', 'difference'];
 const blendMode = blendModes[Math.floor(prngno * blendModes.length)];
 
 
